bf611301152a64f0277e4a4914724cda0e9e1237
Anton Savchuk <savchuk.anton.s@gmail.com>
2021-12-12 22:49:00 +0200

    switching branches

--- TasksInTerminalClass/TaskInTK/task6/task6.c
+++ TasksInTerminalClass/TaskInTK/task6/task6.c
@@ -1,76 +1,27 @@
+#include <stdio.h>
 #ifndef __cplusplus
 #define bool int
 #define true 1
 #define false 0
 #endif
 
-#define RECURS 1
-#define BASE 2
-
-#include <stdio.h>
-#include <math.h>
-
-int div = 0;
-int num;
-
-simple_num(n, div) {
-
-	printf("recursion method: \n");
-
-	if (div == 0)
-		div = n - 1;
-	while (div >= 2) {
-		if (n % div == 0) {
-			printf("Number is not simple\n");
-			return false;
-		}
-		else
-			return simple_num(n, div - 1);
-	}
-
-	return printf("Number is simple");
+bool prime(int num, int del)
+{
+    if (del > num / 2)
+        return true;
+    return num % del ? prime(num, ++del) : false;
 }
 
-int user_choose() {
+int main()
+{
+    int num;
 
-	while (true) {
+    printf("enter your number: ");
+    scanf_s("%i", &num);
 
-		printf("Choose your metod: \n 1 - Recursion method \n 2 - Simple method \n");
-		scanf_s("%d\n", &num);
-		if (num >= RECURS && (num <= BASE))
-			return num;
-
-	}
-
-}
-
-int simple_method(int num) {
-
-	printf("Simple method: \n");
-
-	int sum = 0;
-
-	for (int i = 1; i < num; i++) {
-		if (num % i == 0) {
-			sum++;
-		}
-	}
-
-	if (sum == 2)
-		printf("The number is simple");
-	else
-		printf("The number is not simple");
-}
-
-int main() {
-
-	switch (user_choose())
-	{
-	case RECURS:
-		simple_num(num, div);
-		break;
-	case BASE:
-		simple_method(num);
-		break;
-	}
+    if (prime(num, 2))
+        printf("your number is prime\n");
+    else
+        printf("you number is'nt prime\n");
+    return 0;
 }
