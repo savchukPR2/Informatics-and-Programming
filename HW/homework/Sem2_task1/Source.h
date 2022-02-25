b4d5f5ad889473b75664dee587863adcc272296a
Anton Savchuk <savchuk.anton.s@gmail.com>
2022-02-25 23:30:33 +0300

    changed task1 sem2

---
+++ HW/homework/Sem2_task1/Source.h
@@ -0,0 +1,69 @@
+#define DAY 24
+#define MIN 60
+
+
+
+class Timee
+{
+private:
+	int hour, min, sec;
+
+public:
+	Timee HM(int hour, int min, int sec) { hour = min = sec = 0; }
+
+
+	Timee(const char* str)
+	{
+		string a;
+		int n = 0, ob[3];
+		for (int i = 0; i < (strlen(str) + 1); i++)
+			if (str[i] != ':' && i != strlen(str))
+				a += str[i];
+			else
+			{
+				ob[n] = atoi(a.c_str());
+
+				n++;
+				a = "";
+			}
+		hour = ob[0];
+		min = ob[1];
+		sec = ob[2];
+	}
+
+	void ShowTime()
+	{
+		cout << hour << ":" << min << ":" << sec << endl;
+	}
+
+	void EditTime()
+	{
+		this->hour = (this->hour + (this->min / MIN + this->sec / MIN)) % DAY;
+		this->min = (this->min + this->sec / MIN) % MIN;
+		this->sec %= MIN;
+	}
+
+	Timee& operator += (const Timee& time)
+	{
+		hour += time.hour;
+		min += time.min;
+		sec += time.sec;
+
+		return *this;
+	}
+
+	Timee& operator -= (const Timee& time)
+	{
+		if ((hour - time.hour) < 0)
+			hour = DAY - abs(hour - time.hour);
+		else
+			hour = hour - time.hour;
+		min = abs(min - time.min);
+		sec = abs(sec - time.sec);
+
+		return *this;
+	}
+
+	friend bool operator> (const Timee& time1, const Timee& time2);
+
+};
--- HW/homework/Sem2_task1/main.cpp
+++ HW/homework/Sem2_task1/main.cpp
@@ -1,63 +1,86 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
- 
-#define DAY 24
-#define MIN 60
+#include <W:\Informatics-and-Programming\HW\homework\Sem2_task1\Source.h>
 
-class Timee
+bool operator>(const Timee& time1, const Timee& time2)
 {
-private:
-	int hour, min, sec;
-
-public:
-	Timee() { hour == min == sec == 0; }
-
-	Timee(const char* str)
-	{
-		string a;
-		int n = 0, ob[3];
-		for (int i = 0; i < (strlen(str) + 1); i++)
-			if (str[i] != ':' && i != strlen(str))
-				a += str[i];
-			else
-			{
-				ob[n] = atoi(a.c_str());
-
-				n++;
-				a = "";
-			}
-		hour = ob[0];
-		min = ob[1];
-		sec = ob[2];
-	}
-
-	void ShowTime()
-	{
-		cout << hour << ":" << min << ":" << sec << endl;
-	}
-
-	void EditTime()
-	{
-		this->hour = (this->hour + (this->min / MIN + this->sec / MIN)) % DAY;
-		this->min = (this->min + this->sec / MIN) % MIN;
-		this->sec %= MIN;
-	}
-
-	void PlusTime(const Timee& time)
-	{
-		hour += time.hour;
-		min += time.min;
-		sec += time.sec;
-	}
-	void MinusTime(const Timee& time)
-	{
-		if ((hour - time.hour) < 0)
-			hour = DAY - abs(hour - time.hour);
+	if (time1.hour > time2.hour)
+		return time1.hour > time2.hour;
+	else
+		if (time1.min > time2.min)
+			return time1.min > time2.min;
 		else
-			hour = hour - time.hour;
-		min = abs(min - time.min);
-		sec = abs(sec - time.sec);
-	}
-};
+			if (time1.sec > time2.sec)
+				return time1.sec > time2.sec;
+}
 
+int main()
+{
+	char str[80];
+	int a;
+
+	cout << "Enter Time in (Hour:Min:Sec) format: ";
+	cin >> str;
+
+	while (true)
+	{
+
+		cout << "Enter operation: " << endl << "1) 1 - minus" << endl << "2) 2 - plus" << endl << "3) 3 - compare" << endl << "4) 4 - show time" << endl << "5) 5 - exit" << endl;
+		cin >> a;
+		if (a == 5)
+			break;
+
+		if (a == 4)
+		{
+			Timee ob(str);
+			ob.ShowTime();
+		}
+
+		if (a == 2)
+		{
+			Timee ob(str);
+			cout << "Enter Time in (Hour:Min:Sec) format: ";
+			cin >> str;
+			Timee ob2(str);
+			ob += ob2;
+			ob.EditTime();
+			ob.ShowTime();
+		}
+
+		if (a == 1)
+		{
+			Timee ob(str);
+			cout << "Enter Time in (Hour:Min:Sec) format: ";
+			cin >> str;
+			Timee ob1(str);
+			ob -= ob1;
+			ob.EditTime();
+			ob.ShowTime();
+		}
+
+		if (a == 3)
+		{
+			Timee ob(str);
+			cout << "Enter Time in (Hour:Min:Sec) format: ";
+			cin >> str;
+			Timee ob3(str);
+			if (ob > ob3)
+			{
+				cout << "Main time is greater than yours" << endl;
+			}
+			else
+				cout << "Main time is not greater than yours" << endl;
+		}
+	}
+
+
+
+
+	/*if(ob==ob2)
+	cout<<"ob==ob2";
+	if(ob!=ob1)
+	cout<<"ob!=ob1"; */
+	system("Pause");
+
+}
